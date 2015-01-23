cce237c74412a189b2adee6474df3f9436674791
miked6544 <miked6544@yahoo.com>
2015-01-22 21:25:31 -0500

    paper clip

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -5,6 +5,7 @@ class PinsController < ApplicationController
 
 
 
+
   def index
     @pins = Pin.all
   end
@@ -52,9 +53,6 @@ class PinsController < ApplicationController
       redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?   
     end
   
-
-
-
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
       redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
@@ -63,5 +61,9 @@ class PinsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description, :image)
+    end
+
+    def pin_params
+    params.require(:pin).permit(:description, :image)
     end
 end
