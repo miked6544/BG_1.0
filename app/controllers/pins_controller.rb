69203f7a810110ea7ea3dd1487ddc0242ea4d7cf
miked6544 <miked6544@yahoo.com>
2015-01-23 06:24:01 -0500

    param

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -63,7 +63,4 @@ class PinsController < ApplicationController
       params.require(:pin).permit(:description, :image)
     end
 
-    def pin_params
-    params.require(:pin).permit(:description, :image)
-    end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,13 +82,13 @@ BG10::Application.configure do
 
 
 #Sets Paperclip to upload images to Amazon S3
-  config.paperclip_defaults = {
+# config/environments/production.rb
+config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
+    :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
-
 end
